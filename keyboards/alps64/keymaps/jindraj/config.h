3659fa71dfb23b316948a3ad39da7020f3b1d271
Jakub Jindra <jakub.jindra@socialbakers.com>
2021-10-12 09:43:44 +0200

    [keymap] init alps64 - jindraj

---
+++ keyboards/alps64/keymaps/jindraj/config.h
@@ -0,0 +1,9 @@
+// dynamic macros and turning off features for size reduction
+#define DYNAMIC_MACRO_SIZE 50
+#define DYNAMIC_MACRO_USER_CALL
+#define NO_ACTION_FUNCTION
+#define NO_ACTION_MACRO
+#define NO_ACTION_ONESHOT
+#define TAPPING_FORCE_HOLD
+// i don't have SKCL switch on caps.
+#undef LOCKING_SUPPORT_ENABLE
